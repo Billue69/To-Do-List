b4df0d301caf8d599d799e441536a3da113147fa
Quack <quack@Duck>
2026-08-23 16:57:33 +0500

    Removed useless stuff

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/quack/Documents/Programs/toDoList
-# Build directory: /home/quack/Documents/Programs/toDoList/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
